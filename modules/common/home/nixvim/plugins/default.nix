4eb57a5ff96e5dd8ed291ab09bd70221bd296d22
Jan Erik Töpper <jan.toepper@hornetsecurity.com>
2025-12-03 17:36:49 +0100

    config

--- modules/common/home/nixvim/plugins/default.nix
+++ modules/common/home/nixvim/plugins/default.nix
@@ -17,7 +17,6 @@
   programs.nixvim = {
     plugins = {
       autoclose.enable = true;
-      lspconfig.enable = true;
       tmux-navigator.enable = true;
       ts-autotag.enable = true;
       web-devicons.enable = true;
